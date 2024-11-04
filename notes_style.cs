a89e392c9ad5ceb74d1d957f6949d34aa97ef7c8
sdamkjar <sdamkjar@gmail.com>
2024-11-03 16:19:36 -0800

    Create notes_style.cs

---
+++ notes_style.cs
@@ -0,0 +1,14 @@
+code { background-color: #f4f4f4; border: 1px solid #ddd; border-radius: 4px; padding: 2px 4px; font-family: monospace; font-size: 0.9em; }
+blockquote { background-color: #333; color: #f9f9f9; border-left: 10px solid #ccc; padding: 0.5em; }
+blockquote a { color: #99ccff; }
+blockquote a code { color: #0099ff; }
+blockquote from { color: #99ccff; background: rgba(153, 204, 255, 0.1); border: 1px solid #99ccff; border-radius: 4px; padding: 2px 2px; font-size: 0.8em; }
+blockquote from b { color: white; background: #99ccff; border-radius: 4px; padding: 3px 5px 2px; margin-left: -3px; }
+blockquote to { color: #ff99cc; background: rgba(255, 153, 204, 0.1); border: 1px solid #ff99cc; border-radius: 4px; padding: 2px 2px; font-size: 0.8em; }
+blockquote to b { color: white; background: #ff99cc; border-radius: 4px; padding: 3px 5px 2px; margin-left: -3px; }
+blockquote subject { color: #f2f2f2; background: #555555; border-radius: 4px; padding: 2px 2px; font-size: 0.8em; }
+blockquote subject b { color: #555555; background: #f2f2f2; border-radius: 4px; padding: 3px 5px 2px; margin-left: -3px; }
+todo { color: #ff6600; background: rgba(255, 102, 0, 0.1); border: 1px solid #ff6600; border-radius: 4px; padding: 2px 2px; }
+todo b { color: white; background: #ff6600; border-radius: 4px; padding: 3px 5px 2px; margin-left: -4px; }
+done { color: #009900; background: rgba(0, 153, 0, 0.1); border: 1px solid #009900; border-radius: 4px; padding: 2px 2px; }
+done b { color: white; background: #009900; border-radius: 4px; padding: 3px 5px 2px; margin-left: -4px; }
